2e9c38840b38bcf89cf9c8bbde8d3e6aa22c623d
InfoFractal <66846724+InfoFractal@users.noreply.github.com>
2020-06-12 16:04:16 -0400

    Add files via upload

--- lang/es/block_if_sence_login.php
+++ lang/es/block_if_sence_login.php
@@ -1,1 +1,12 @@
-
+<?php
+//Textos del Bloque
+$string['pluginname'] = 'Autenticación Sence';
+$string['sencelogin'] = 'Autenticación Sence';
+$string['sencelogin:addinstance'] = 'Agrega un nuevo bloque de autenticación';
+$string['sencelogin:myaddinstance'] = 'Agrega un nuevo bloque de autenticación a mi p&aacute;gina';
+//Textos del Administrador
+$string['adminpageheading'] = 'Información OTEC';
+$string['adminsettingheading'] = 'Datos requeridos por la plataforma SENCE';
+$string['runotec'] = 'RUN OTEC';
+$string['tokenotec'] = 'Token';
+$string['urlerror'] = "URL Error";
