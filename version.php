9f9b89b499ef8dc3c15078824738baea6629aaa2
InfoFractal <contacto@infofractal.io>
2020-06-13 00:42:39 -0400

    Agregada dependencia para plugin tool_coursefields

--- version.php
+++ version.php
@@ -27,3 +27,4 @@
 $plugin->component = 'block_if_sence_login';  // Recommended since 2.0.2 (MDL-26035). Required since 3.0 (MDL-48494)
 $plugin->version = 2020060909;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->dependencies = array('tool_coursefields' => ANY_VERSION);
