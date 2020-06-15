252d29a33db6fc89d52be54dd5d038a8f518cc3c
Daniel Torres <danieltorres@MacBook-Pro-de-Daniel-2.local>
2020-06-15 18:37:35 -0400

    Se elimina campos de URLError y URLRetorno. Quedan armadas por defecto en el sistema. Se designa url de retorno a la url del curso

--- settings.php
+++ settings.php
@@ -35,7 +35,6 @@ if($hassiteconfig){
             get_string('runotec','block_if_sence_login'), '', ''));
     $settings->add(new admin_setting_configtext('block_if_sence_login/tokenotec', 
             get_string('tokenotec','block_if_sence_login'), '', ''));
-    $settings->add(new admin_setting_configtext('block_if_sence_login/urlerror', 
-            get_string('urlerror','block_if_sence_login'), '', ''));
+    
     //$ADMIN->add('messaging',$settings);
 }
